777f8198769f8e98510aa8b810f882b3a167cb00
hxxtae <fkdlxmfkdl@gmail.com>
2024-10-03 17:58:52 +0900

    fix: Added total price and list style

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
   const [products, setProducts] = useState<MockData[]>([])
   const [page, setPage] = useState<number>(1)
   const lastValidate = Math.ceil(products.length / LIMIT)
+  const totalPrice = products
+    .slice(0, page * LIMIT)
+    .reduce((prev, product) => prev + product.price, 0)
+  const totalCnt = products.slice(0, page * LIMIT).length
 
   const onNextList = () => {
     setPage((prev) => {
@@ -29,19 +33,46 @@ function App() {
 
   return (
     <main className="main">
-      <section className="product_section">
-        <ul className="product_list">
-          {products.length > 0 &&
-            products
-              .slice(0, page * LIMIT)
-              .map((product) => (
-                <ProductCard key={product.productId} product={product} />
-              ))}
-          {lastValidate !== page && (
-            <IntersectionContainer onReFetch={onNextList} />
-          )}
-        </ul>
-      </section>
+      <div className="container">
+        <article className="side_article pc">
+          <div className="side_box">
+            <div className="box_wrapper">
+              <span>count:&nbsp;</span>
+              <p>{totalCnt.toLocaleString()}</p>
+            </div>
+            <div className="box_wrapper">
+              <span>price:&nbsp;</span>
+              <p>${totalPrice.toLocaleString()}</p>
+            </div>
+          </div>
+        </article>
+        <section className="product_section">
+          <ul className="product_list">
+            {products.length > 0 &&
+              products
+                .slice(0, page * LIMIT)
+                .map((product) => (
+                  <ProductCard key={product.productId} product={product} />
+                ))}
+            {lastValidate !== page && (
+              <IntersectionContainer onReFetch={onNextList} />
+            )}
+          </ul>
+        </section>
+      </div>
+
+      <article className="side_article mobile">
+        <div className="side_box">
+          <div className="box_wrapper">
+            <span>count:&nbsp;</span>
+            <p>{totalCnt.toLocaleString()}</p>
+          </div>
+          <div className="box_wrapper">
+            <span>price:&nbsp;</span>
+            <p>${totalPrice.toLocaleString()}</p>
+          </div>
+        </div>
+      </article>
     </main>
   )
 }
